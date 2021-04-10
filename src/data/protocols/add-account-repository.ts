241d6a7c29b60b2480b1b0060e5587685be4cd65
JoaoPGoes <joaopedro.goes13@gmail.com>
2021-04-10 17:26:32 -0300

    feat: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/data/protocols/add-account-repository.ts
+++ src/data/protocols/add-account-repository.ts
@@ -1,5 +1,5 @@
-import { AddAccountModel } from '../../domain/usecases/add-account'
 import { AccountModel } from '../../domain/models/Account'
+import { AddAccountModel } from '../../domain/usecases/add-account'
 
 export interface AddAccountRepository {
   add (accountData: AddAccountModel): Promise<AccountModel>
