6c8a4de812023b87b302cc1d6f427d266b6e0171
João Pedro Goes <joaopedro.goes13@gmail.com>
2021-07-18 13:51:48 -0300

    refactor: rename TokenGenerator to Encrypter

--- src/data/usecases/authentication/db-authetication.ts
+++ src/data/usecases/authentication/db-authetication.ts
@@ -4,24 +4,24 @@ import {
   LoadAccountByEmailRepository,
   UpdateAcessTokenRepository,
   HashComparer,
-  TokenGenerator
+  Encrypter
 } from './db-authentication-protocols'
 
 export class DbAuthentication implements Authetication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
-  private readonly tokenGenerator: TokenGenerator
+  private readonly encrypter: Encrypter
   private readonly updateAcessTokenRepository: UpdateAcessTokenRepository
 
   constructor (
     loadAccountByEmailRepository: LoadAccountByEmailRepository,
     hashComparer: HashComparer,
-    tokenGenerator: TokenGenerator,
+    encrypter: Encrypter,
     updateAcessTokenRepository: UpdateAcessTokenRepository
   ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
-    this.tokenGenerator = tokenGenerator
+    this.encrypter = encrypter
     this.updateAcessTokenRepository = updateAcessTokenRepository
   }
 
@@ -30,7 +30,7 @@ export class DbAuthentication implements Authetication {
     if (account) {
       const isValid = await this.hashComparer.compare(authentication.password, account.password)
       if (isValid) {
-        const accessToken = await this.tokenGenerator.generate(account.id)
+        const accessToken = await this.encrypter.encrypt(account.id)
         await this.updateAcessTokenRepository.update(account.id, accessToken)
         return accessToken
       }
